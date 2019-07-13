e61b59f02d413d014431272f13b4e96eba8ed851
Draymond-TS <846933717@qq.com>
2019-07-13 23:03:34 +0800

    使用SDK生成预览地址

--- src/main/java/life/draymond/community/Provider/UCloudProvider.java
+++ src/main/java/life/draymond/community/Provider/UCloudProvider.java
@@ -7,6 +7,8 @@ import cn.ucloud.ufile.auth.UfileObjectLocalAuthorization;
 import cn.ucloud.ufile.bean.PutObjectResultBean;
 import cn.ucloud.ufile.exception.UfileClientException;
 import cn.ucloud.ufile.exception.UfileServerException;
+import life.draymond.community.exception.CustomizeErrorCode;
+import life.draymond.community.exception.CustomizeException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +21,14 @@ public class UCloudProvider {
     private String publicKey;
     @Value("${ucloud.ufile.private-key}")
     private String privateKey;
+    private String bucketName="draymond";
+
 
     public String upload(InputStream fileStream, String mimeType, String fileName) {
         String generatedFileName;
         String [] filePaths=fileName.split("\\.");
         if(filePaths.length>1){
-            generatedFileName=UUID.randomUUID().toString() +"."+filePaths[1];
+            generatedFileName=UUID.randomUUID().toString() +"."+filePaths[filePaths.length-1];
         }else{
             return null;
         }
@@ -37,7 +41,7 @@ public class UCloudProvider {
             PutObjectResultBean response= UfileClient.object(objectAuthorization,config)
                     .putObject(fileStream,mimeType)
                     .nameAs(generatedFileName)
-                    .toBucket("draymond")
+                    .toBucket(bucketName)
                     /**
                      * 是否上传校验MD5, Default = true
                      */
@@ -50,18 +54,26 @@ public class UCloudProvider {
                      * 配置进度监听
                      */
                     .setOnProgressListener((bytesWritten,contentLength)->{
-
                     })
                     .execute();
+
+                    if(response !=null && response.getRetCode()==0){
+                        String url=UfileClient.object(objectAuthorization,config)
+                                .getDownloadUrlFromPrivateBucket(generatedFileName,bucketName,24*60*60)
+                                .createUrl();
+                        return url;
+
+                    }else {
+                        throw  new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
+                    }
         }catch (UfileClientException e) {
             e.printStackTrace();
-            return null;
+            throw  new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
         } catch (UfileServerException e) {
             e.printStackTrace();
-            return null;
+            throw  new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
         }
 
-        return generatedFileName;
     }
 
 }
--- src/main/java/life/draymond/community/controller/FileController.java
+++ src/main/java/life/draymond/community/controller/FileController.java
@@ -3,6 +3,8 @@ package life.draymond.community.controller;
 import com.sun.org.apache.bcel.internal.generic.NEW;
 import life.draymond.community.Provider.UCloudProvider;
 import life.draymond.community.dto.FileDTO;
+import life.draymond.community.exception.CustomizeErrorCode;
+import life.draymond.community.exception.CustomizeException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,16 +28,16 @@ public class FileController {
         //获取request里面的参数
         MultipartFile file = multipartRequest.getFile("editormd-image-file");
         try {
-            uCloudProvider.upload(file.getInputStream(),file.getContentType(),file.getOriginalFilename());
+            String fileName = uCloudProvider.upload(file.getInputStream(), file.getContentType(), file.getOriginalFilename());
+            FileDTO fileDTO = new FileDTO();
+            fileDTO.setSuccess(1);
+            fileDTO.setUrl(fileName);
+            return fileDTO;
         } catch (IOException e) {
             e.printStackTrace();
+            throw new CustomizeException(CustomizeErrorCode.FILE_UPLOAD_FAIL);
         }
 
-        FileDTO fileDTO = new FileDTO();
-        fileDTO.setSuccess(1);
-        fileDTO.setUrl("/images/wechat.png");
-
-        return fileDTO;
     }
 
 }
